a8fabfcd2e6d1c8c8aa1480f41e7224116558668
andrasvas <asulismodszer@gmail.com>
2025-02-17 11:30:56 +0100

    szükséges modulok importálása
    bootstrap, router, tachyon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import { useState } from 'react'
+import React from "react"
+import Home from "./oldalak/Home"
+import Navbar from "./Navbar"
+import Mermesek from "./oldalak/Mermesek"
+import Ermesek from "./oldalak/Ermesek"
+import {Route, Routes} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "tachyons"
 
 function App() {
   return (
---
+++ src/oldalak/Mermesek.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Mermesek = (() =>{
+
+    return(
+        <div>
+            <h1>Az olimpia magyar érmesei</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus rerum repellat reprehenderit illum atque alias molestias eos autem illo! Eligendi nesciunt ipsa voluptatibus, error accusantium, itaque mollitia quo magni, hic fugit harum. Repudiandae mollitia veritatis vel nemo iure aliquid laudantium tempora hic in, molestiae eligendi. Voluptatibus, voluptatem molestiae! Velit voluptatibus neque libero officiis voluptates ea ad exercitationem fugiat totam quisquam magni expedita molestias blanditiis consequatur odio vel ab adipisci temporibus natus pariatur, facilis nobis nostrum numquam? Illum nemo commodi similique maiores qui, harum cum voluptates et dicta aliquam. In, est deleniti ipsam beatae ipsa soluta vitae aspernatur perspiciatis officiis quasi.</p>
+        </div>
+    )
+
+}
+)
+
+export default Mermesek
